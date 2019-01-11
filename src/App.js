88bfeec78023d81c75952bae42fa5cacbff042ac
blue_baney <bluebaney10@gmail.com>
2019-01-11 17:17:01 +0700

    hide config

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import MessageBox from 'components/MessageBox';
 import firebase from 'firebase';
 
 class App extends Component {
+ 
+ 
 
+  constructor(props){super(props);
+    var data = require('data/config.json'); 
 
-  constructor(props){super(props);var config = {
-    apiKey: "AIzaSyDhsrx2t-z4ENeX3P7ki9769pk951bweoA",
-    authDomain: "react-firebase-7dede.firebaseapp.com",
-    databaseURL: "https://react-firebase-7dede.firebaseio.com",
-    projectId: "react-firebase-7dede",
-    storageBucket: "",
-    messagingSenderId: "442908432269"
+    var obj = data[0];
+
+   
+    var config = {
+    apiKey: obj.apiKey,
+    authDomain: obj.authDomain,
+    databaseURL: obj.databaseURL,
+    projectId: obj.projectId,
+    storageBucket: obj.storageBucket,
+    messagingSenderId: obj.messagingSenderId
   };
   firebase.initializeApp(config);
 }
